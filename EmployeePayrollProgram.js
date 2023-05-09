67729f7e9cc7c01a93fcf74c743d6a53f5bcaa1d
shephali09 <hiremathshephali9@gmail.com>
2023-05-09 10:41:15 +0530

    Add: Ability to Check Employee is present or Absent

--- EmployeePayrollProgram.js
+++ EmployeePayrollProgram.js
@@ -1,1 +1,12 @@
 console.log("Welcome to Employee Payroll Program!!");
+
+//UC1
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() *10) % 2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is absent");
+    return;
+} else {
+    console.log("Employee is present");
+    return;
+}
