999a4d83078428a7816d5c99523d6fa3e6299a14
shephali09 <hiremathshephali9@gmail.com>
2023-05-09 11:12:11 +0530

    Add: Calculating Wages for a Month assuming 20 Working Days in a Month

--- EmployeePayrollProgram.js
+++ EmployeePayrollProgram.js
@@ -1,12 +1,13 @@
 console.log("Welcome to Employee Payroll Program!!");
 
-//UC3
+//UC4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const IS_NO_TIME = 0;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -20,7 +21,10 @@ function getWorkingHours(empCheck) {
     }
 }
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
 let employeeWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Daily Wage: " + employeeWage);
+console.log("Total Hours: " + empHrs + " Employee Daily Wage: " + employeeWage);
