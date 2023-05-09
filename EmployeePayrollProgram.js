9bf9cb1cb122974ce6d2b31f4532d1a28f86ead9
shephali09 <hiremathshephali9@gmail.com>
2023-05-09 11:03:22 +0530

    Refactor: Ability to Calculate Daily Employee Wage based on part time or full time work

--- EmployeePayrollProgram.js
+++ EmployeePayrollProgram.js
@@ -1,23 +1,26 @@
 console.log("Welcome to Employee Payroll Program!!");
 
-//UC2
+//UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const IS_NO_TIME = 0;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() *10) % 3;
-switch(empCheck){
-      case IS_PART_TIME :
-        empHrs = PART_TIME_HOURS;
-        break;
-     case IS_FULL_TIME :
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default :
-        empHrs = IS_NO_TIME;
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let employeeWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Daily Wage: " + employeeWage);
