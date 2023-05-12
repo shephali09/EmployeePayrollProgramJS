27ce51171f3956b3c10357c73f208df0a365e8a0
shephali09 <hiremathshephali9@gmail.com>
2023-05-13 00:53:22 +0530

    Add:Store the Day and the Daily Wage along with the Total Wage

--- EmployeePayrollProgram.js
+++ EmployeePayrollProgram.js
@@ -115,3 +115,12 @@ let totalDaysWorkedArray = mapDayWithWageArray.reduce(totalDaysWorked, 0);
 console.log("UC-7G - Number of days employee worked: ");
 console.log(totalDaysWorkedArray);
 
+//UC8
+console.log("UC8 - Store the Day and the Daily Wage along with the Total Wage");
+console.log(empDailyWageMap);
+let totalWageUsingMap = 0;
+function totalWagesMap(dailyWage) {
+    totalWageUsingMap += dailyWage;
+}
+Array.from(empDailyWageMap.values()).map(totalWagesMap);
+console.log(" Total Wage using Map: " + totalWageUsingMap);
