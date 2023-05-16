05af6f93064a4e8b8b923b01d8a26bd09d2e24bc
shephali09 <hiremathshephali9@gmail.com>
2023-05-16 11:23:24 +0530

    Add: Ability to check the name starts with capital and has at least 3 characters - Use Regex Pattern - Use Try Catch in case of Error

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,4 +1,4 @@
-class EmployeePayrollData{
+class EmployeePayrollData {
     //properties
     //id;
     //name;
@@ -7,50 +7,61 @@ class EmployeePayrollData{
     startDate;
 
     //constructor
-    constructor(...params){
-        this.id=params[0];
-        this.name=params[1];
-        this.salary=params[2];
-        this.gender=params[3];
-        this.startDate=params[4];
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
 
     //getter and setter methods
-    get name(){
+    get name() {
         return this._name;
     }
-    set name(name){
-        this._name = name;
+    set name(name) {
+        let nameRegex = RegExp('[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else console.log("Name is Incorrect");
+
     }
-    get id(){
+    get id() {
         return this._id;
     }
-    set id(id){
+    set id(id) {
         this._id = id;
     }
-    get salary(){
+    get salary() {
         return this._salary;
     }
-    set salary(salary){
+    set salary(salary) {
         this._salary = salary;
     }
 
     //method
-    toString(){
-        const options = { year: 'numeric', month: 'long', day:'numeric'};
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const empDate = this.startDate === undefined ? "undefined" :
-        this.startDate.toLocaleDateString("en-US", options);
+            this.startDate.toLocaleDateString("en-US", options);
 
-        return "id=" +this.id + " ,name=" + this.name + ",salary=" + this.salary + " gender=" +this.gender + ",start Date=" +empDate;
+        return "id=" + this.id + " ,name=" + this.name + ",salary=" + this.salary + " gender=" + this.gender + ",start Date=" + empDate;
     }
 
 }
 
-let employeePayrollData = new EmployeePayrollData(1,"Shephali",45699);
+let employeePayrollData = new EmployeePayrollData(1, "Shephali", 45699);
 console.log(employeePayrollData.toString());
 employeePayrollData.id = 2;
-employeePayrollData.name = "Shreya";
+try {
+    employeePayrollData.name = "Shephali";
+    console.log(employeePayrollData.toString());
+} catch (e) {
+    console.error("the name should starts with capital and has at least 3 characters");
+
+}
+
 employeePayrollData.salary = 30000;
 console.log(employeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrollData(1,"Shephali",45699,"F",new Date());
+let newEmployeePayrollData = new EmployeePayrollData(1, "Shephali", 45699, "F", new Date());
 console.log(newEmployeePayrollData.toString());
