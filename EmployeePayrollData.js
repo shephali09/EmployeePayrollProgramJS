f1f08d79f5ae129a30f46c304c9edd2b59462a96
shephali09 <hiremathshephali9@gmail.com>
2023-05-17 16:23:14 +0530

    Add: Ability to check the employee id and salary are non zero positive number, the gender is M or F and date is not a future date- Use Regex Pattern- Use Try Catch in case of Error

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,10 +3,18 @@ class EmployeePayrollData {
     //id;
     //name;
     //salary;
-    gender;
-    startDate;
+    //gender;
+    //startDate;
 
     //constructor
+
+     //constructor(id,name,salary) {
+       //  this.id = id;
+       //  this.name = name;
+       //    this.salary = salary;
+
+     //}
+
     constructor(...params) {
         this.id = params[0];
         this.name = params[1];
@@ -16,36 +24,54 @@ class EmployeePayrollData {
     }
 
     //getter and setter methods
-    get name() {
-        return this._name;
+    getName() {
+        return this.name;
     }
-    set name(name) {
+    setName(name) {
         let nameRegex = RegExp('[A-Z]{1}[a-z]{3,}$');
         if (nameRegex.test(name))
-            this._name = name;
-        else console.log("Name is Incorrect");
+            this.name = name;
+        else throw "Name is Incorrect";
 
     }
-    get id() {
-        return this._id;
+    getId() {
+        return this.id
+    };
+    setId(id) {
+        this.id = id;
     }
-    set id(id) {
-        this._id = id;
+    getSalary() {
+        return this.salary
+    };
+    setSalary(salary) {
+        if (salary > 0)
+            this.salary = salary;
+        else throw "Salary is Invalid";
     }
-    get salary() {
-        return this._salary;
+    getGender() {
+        return this.gender
+    };
+    setGender(gender) {
+        if (gender == "M" || gender == "F")
+            this.gender = gender;
+        else throw "Gender is Incorrect";
     }
-    set salary(salary) {
-        this._salary = salary;
+    getStartDate() {
+        return this.startDate
+    };
+    setStartDate(startDate) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = startDate === undefined ? "undefined" :
+            startDate.toLocaleDateString("en-US", options);
+
+        if ((new Date(empDate) <= new Date()))
+            this.startDate = empDate;
+        else throw "Invalid Date";
     }
 
     //method
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = this.startDate === undefined ? "undefined" :
-            this.startDate.toLocaleDateString("en-US", options);
-
-        return "id=" + this.id + " ,name=" + this.name + ",salary=" + this.salary + " gender=" + this.gender + ",start Date=" + empDate;
+        return "id=" + this.id + " ,name=" + this.name + " ,salary=" + this.salary + " ,gender=" + this.gender + " ,start Date=" + this.startDate;
     }
 
 }
@@ -63,5 +89,26 @@ try {
 
 employeePayrollData.salary = 30000;
 console.log(employeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrollData(1, "Shephali", 45699, "F", new Date());
+let newEmployeePayrollData = new EmployeePayrollData(2, "Shephali", 45699, "F", new Date());
 console.log(newEmployeePayrollData.toString());
+
+let employeeOne = new EmployeePayrollData();
+employeeOne.setId(3);
+employeeOne.setName("Harshad");
+employeeOne.setSalary(50000);
+employeeOne.setGender('M');
+employeeOne.setStartDate(new Date('5/16/2023'));
+console.log(employeeOne.toString());
+
+try {
+    employeeOne.setGender("Male");
+    console.log(employeeOne.toString());
+} catch (e) {
+    console.error(e);
+}
+try {
+    employeeOne.setStartDate(new Date('16/05/2023'));
+    console.log(employeeOne.toString());
+} catch (e) {
+    console.error(e);
+}
